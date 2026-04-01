ba6d389a2065489536b605a5d26f6750d11c20e1
Paulina <paulina.bojes@gmail.com>
2026-04-01 21:20:30 +0200

    Initial commit. Ingredient model.

---
+++ Kitchen.Api/Models/Entities/Ingredient.cs
@@ -0,0 +1,10 @@
+﻿namespace Kitchen.Api.Models.Entities
+{
+    public class Ingredient
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public double Amount { get; set; }
+        public string Unit { get; set; } = string.Empty; // g/ml/szt
+    }
+}

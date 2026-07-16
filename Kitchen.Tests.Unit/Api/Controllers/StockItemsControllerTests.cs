e2e2f38627a90483a32178a46e5ded84f7451ec8
Paulina <paulina.bojes@gmail.com>
2026-07-16 19:12:43 +0200

    test(api): cover StockItemsController actions
    Covers GetAll, Get by id (found and not-found), Get by name (found
    and not-found), Update, and Delete.

--- Kitchen.Tests.Unit/Api/Controllers/StockItemsControllerTests.cs
+++ Kitchen.Tests.Unit/Api/Controllers/StockItemsControllerTests.cs
@@ -2,6 +2,7 @@
 using Kitchen.Application.Commands;
 using Kitchen.Application.Models.Requests;
 using Kitchen.Application.Services;
+using Kitchen.Core.Domain.Entities;
 using Kitchen.Core.Domain.Enums;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
@@ -11,6 +12,11 @@ namespace Kitchen.Tests.Unit.Api.Controllers
 {
     public class StockItemsControllerTests
     {
+        private const string ValidName = "Pomidor";
+        private const string ValidName2 = "Cebula";
+        private const int ValidAmount = 5;
+        private const StorageLocation ValidLocation = StorageLocation.Fridge;
+
         private readonly Mock<IInventoryService> _inventoryServiceMock;
         private readonly StockItemsController _controller;
 
@@ -26,9 +32,9 @@ namespace Kitchen.Tests.Unit.Api.Controllers
             // Arrange
             var request = new CreateStockItemRequest
             {
-                Name = "Tomato",
-                Amount = 5,
-                Location = StorageLocation.Fridge
+                Name = ValidName,
+                Amount = ValidAmount,
+                Location = ValidLocation
             };
 
             // Act
@@ -46,5 +52,131 @@ namespace Kitchen.Tests.Unit.Api.Controllers
                 c.Amount == request.Amount &&
                 c.Location == request.Location)), Times.Once);
         }
+
+        #region GetAll
+
+        [Fact]
+        public async Task GetAll_ShouldReturnOk_WithStockItems()
+        {
+            var expected = new List<StockItem>
+            {
+                new(ValidName, ValidAmount, ValidLocation, null),
+                new(ValidName2, ValidAmount, ValidLocation, null)
+            };
+
+            _inventoryServiceMock
+                .Setup(s => s.GetAll())
+                .ReturnsAsync(expected);
+
+            var response = await _controller.GetAll();
+
+            var result = response.Should().BeOfType<OkObjectResult>().Subject;
+            result.Value.Should().BeEquivalentTo(expected);
+            _inventoryServiceMock.Verify(s => s.GetAll(), Times.Once);
+        }
+
+        #endregion
+
+        #region Get by id
+
+        [Fact]
+        public async Task GetById_ShouldReturnOk_WhenStockItemExists()
+        {
+            var stockItem = new StockItem(ValidName, ValidAmount, ValidLocation, null);
+
+            _inventoryServiceMock
+                .Setup(s => s.GetById(stockItem.Id.Value))
+                .ReturnsAsync(stockItem);
+
+            var response = await _controller.Get(stockItem.Id.Value);
+
+            var result = response.Should().BeOfType<OkObjectResult>().Subject;
+            result.Value.Should().Be(stockItem);
+        }
+
+        [Fact]
+        public async Task GetById_ShouldReturnNotFound_WhenStockItemDoesNotExist()
+        {
+            _inventoryServiceMock
+                .Setup(s => s.GetById(It.IsAny<Guid>()))
+                .ReturnsAsync((StockItem?)null);
+
+            var response = await _controller.Get(Guid.NewGuid());
+
+            response.Should().BeOfType<NotFoundResult>();
+        }
+
+        #endregion
+
+        #region Get by name
+
+        [Fact]
+        public async Task GetByName_ShouldReturnOk_WhenStockItemsExist()
+        {
+            var name = "Ziemniak";
+            var expected = new List<StockItem> { new(name, ValidAmount, ValidLocation, null) };
+
+            _inventoryServiceMock
+                .Setup(s => s.GetByName(name))
+                .ReturnsAsync(expected);
+
+            var response = await _controller.Get(name);
+
+            var result = response.Should().BeOfType<OkObjectResult>().Subject;
+            result.Value.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public async Task GetByName_ShouldReturnNotFound_WhenNoStockItemsMatchName()
+        {
+            _inventoryServiceMock
+                .Setup(s => s.GetByName(It.IsAny<string>()))
+                .ReturnsAsync(Enumerable.Empty<StockItem>());
+
+            var response = await _controller.Get("Nieznany");
+
+            response.Should().BeOfType<NotFoundResult>();
+        }
+
+        #endregion
+
+        #region Update
+
+        [Fact]
+        public async Task Update_ShouldReturnNoContent_WhenValid()
+        {
+            var id = Guid.NewGuid();
+            var request = new UpdateStockItemRequest
+            {
+                Amount = ValidAmount,
+                Location = ValidLocation
+            };
+
+            var response = await _controller.Update(id, request);
+
+            response.Should().BeOfType<NoContentResult>();
+
+            _inventoryServiceMock.Verify(s => s.Update(It.Is<ModifyStockItemCommand>(c =>
+                c.Id == id &&
+                c.Amount == request.Amount &&
+                c.Location == request.Location)), Times.Once);
+        }
+
+        #endregion
+
+        #region Delete
+
+        [Fact]
+        public async Task Delete_ShouldReturnNoContent_WhenSuccessful()
+        {
+            var id = Guid.NewGuid();
+
+            var response = await _controller.Delete(id);
+
+            response.Should().BeOfType<NoContentResult>();
+            _inventoryServiceMock.Verify(s => s.Delete(id), Times.Once);
+        }
+
+        #endregion
     }
 }
